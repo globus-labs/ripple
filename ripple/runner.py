a25f8ecaa1a6716f0c4bd06193df3e87beae4267
ryanchard <ryan@chard.co.nz>
2017-10-02 16:32:48 -0500

    Removing debug.

--- ripple/runner.py
+++ ripple/runner.py
@@ -75,7 +75,7 @@ class RippleRunner():
         try:
             r = requests.post(RippleConfig().get_jobs_path, data=payload)
             data = json.loads(r.text)
-            logger.debug("got data?")
+            
         except Exception as e:
             logger.error("Did not receive proper json when polling for jobs.")
             logger.error(e)
